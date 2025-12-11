172e223ae816d5f5c81fd7986a9f23bc9de7c90f
Bhargavi-kona <24331a05e1@mvgrce.edu.in>
2025-12-11 19:09:38 +0530

    Add files via upload

---
+++ math.py
@@ -0,0 +1,8 @@
+import math
+print("Value of pi:", math.pi)
+num = 25
+print("Square root of", num, "is:", math.sqrt(num))
+angle = 30  
+radians = math.radians(angle)  
+print("sin(30°) =", math.sin(radians))
+print("cos(30°) =", math.cos(radians))
